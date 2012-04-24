7d23712c4e01a835a9ebe90905b902c6eb5693f3
antongrau <alar@soc.ku.dk>
2012-04-24 14:44:27 +0200

    A new adjustment method that gives much better results. Changes have been made to soc.ca, subset.ca, print.soc.ca and scree. These changes have not been properly tested.
    As a result of these changes Issue #2 has finally been closed.
    
    Signed-off-by: antongrau <alar@soc.ku.dk>

--- functions_analysis.r
+++ functions_analysis.r
@@ -36,77 +36,88 @@
 #'
 
 soc.ca <- function(x, sup=NULL, identifier=NULL, passive="Missing"){
-
-x       <- data.frame(lapply(x, factor))            # turn active variables into factor
-a.r     <- nrow(x)                                  # Number of active rows or the number of individuals
-sup.n   <- sum(unlist(lapply(as.data.frame(sup), nlevels))) # Number of supplementary modalities
-
-B           <- burt(x)                              # The active burt matrix is created    
-supmat <- matrix(, nrow=0, ncol=ncol(B))
-
-if (identical(sup, NULL)==FALSE){
-sup     <- data.frame(lapply(sup, factor))          # turn supplementary variables into factor
-supmat      <- sup.burt(x, sup)                     # The supplementary burt matrix is created
-}                                                    
-
-if (identical(identifier, NULL)==FALSE){
-supmat                <- rbind(supmat, indicator(x, id=identifier)) # The supplementary indicatormatrix is created
-}
-
-if ((identical(identifier, NULL)==TRUE)&(identical(sup, NULL)==TRUE)){
-supmat <- matrix(, nrow=2, ncol=ncol(B))
-rownames(supmat) <- c("No Supplementary points", "No Supplementary points" )
-}
-
-sub         <- grepl(paste(passive, collapse="|"),colnames(B))
-set         <- 1:ncol(B)
-subset      <- set[!sub]
-
-result      <- subset.ca(B, subset, supmat, nvar=ncol(x))
-
-l.active    <- length(subset)
-l.id        <- nlevels(identifier)
-l.sup 	    <- nrow(supmat)-l.id
-i.active    <- (1:l.active)
-i.sup	    <- (l.active+1):(l.active+l.sup)
-i.id 	    <- (l.active+l.sup+1):(l.active+l.sup+l.id)
-
-# List of descriptive values
-
-result$nid      <- a.r
-result$npassive <- length(which(sub==TRUE)) 
-result$passive  <- colnames(B)[sub] # Could be a matrix with the amount of individuals in each modality
-
-result$active 	<- i.active
-result$sup		<- i.sup
-result$identifier	<- i.id
-result$Burt 		<- B[subset, subset] 
-
-varnames    <- colnames(x)
-ml          <- vector()
-for (i in 1:ncol(x)){
- ml         <- c(ml, rep(varnames[i], nlevels(x[,i])))
-}
-ml          <- ml[!sub]
-mm          <- as.matrix(cbind(ml, 1:length(ml)))
-md          <- matrix(, nrow=ncol(x), ncol=3)
-rownames(md) <- varnames
-colnames(md) <- c("Start", "End", "Modalities")
-md          <- as.data.frame(md)
-
-for (i in 1:ncol(x)){
- mr         <- as.numeric(mm[,2][mm[,1]==varnames[i]])
- md[i,1]    <- min(mr)
- md[i,2]    <- max(mr)
- md[i,3]    <- length(mr)
-}
-md[,1]      <- as.numeric(md[,1])
-md[,2]      <- as.numeric(md[,2])
-
-result$modal    <- md
-
-class(result)   <- "soc.ca"
-return(result)
+  
+  x       <- data.frame(lapply(x, factor))            # turn active variables into factor
+  a.r     <- nrow(x)                                  # Number of active rows or the number of individuals
+  sup.n   <- sum(unlist(lapply(as.data.frame(sup), nlevels))) # Number of supplementary modalities
+  
+  B           <- burt(x)                              # The active burt matrix is created    
+  supmat <- matrix(, nrow=0, ncol=ncol(B))
+  
+  if (identical(sup, NULL)==FALSE){
+    sup     <- data.frame(lapply(sup, factor))          # turn supplementary variables into factor
+    supmat      <- sup.burt(x, sup)                     # The supplementary burt matrix is created
+  }                                                    
+  
+  if (identical(identifier, NULL)==FALSE){
+    supmat                <- rbind(supmat, indicator(x, id=identifier)) # The supplementary indicatormatrix is created
+  }
+  
+  if ((identical(identifier, NULL)==TRUE)&(identical(sup, NULL)==TRUE)){
+    supmat <- matrix(, nrow=2, ncol=ncol(B))
+    rownames(supmat) <- c("No Supplementary points", "No Supplementary points" )
+  }
+  
+  sub         <- grepl(paste(passive, collapse="|"),colnames(B))
+  set         <- 1:ncol(B)
+  subset      <- set[!sub]
+  
+  # Finds the amount of variables without passive modalities
+  Qm    <- ncol(x)
+  for (i in seq(ncol(x))){
+    lev <- levels(x[,i])
+    pasQ <- grepl(paste(passive, collapse="|"), lev)
+    if (any(pasQ==TRUE)==TRUE){
+      Qm <- Qm - 1
+    }
+  }
+  
+  
+  result      <- subset.ca(B, subset, supmat, nvar=ncol(x), Qm=Qm)
+  
+  l.active    <- length(subset)
+  l.id        <- nlevels(identifier)
+  l.sup       <- nrow(supmat)-l.id
+  i.active    <- (1:l.active)
+  i.sup	    <- (l.active+1):(l.active+l.sup)
+  i.id 	    <- (l.active+l.sup+1):(l.active+l.sup+l.id)
+  
+  # List of descriptive values
+  
+  result$nid      <- a.r
+  result$npassive <- length(which(sub==TRUE)) 
+  result$passive  <- colnames(B)[sub] # Could be a matrix with the amount of individuals in each modality
+  
+  result$active 	<- i.active
+  result$sup		<- i.sup
+  result$identifier	<- i.id
+  result$Burt 		<- B[subset, subset] 
+  
+  varnames    <- colnames(x)
+  ml          <- vector()
+  for (i in 1:ncol(x)){
+    ml         <- c(ml, rep(varnames[i], nlevels(x[,i])))
+  }
+  ml          <- ml[!sub]
+  mm          <- as.matrix(cbind(ml, 1:length(ml)))
+  md          <- matrix(, nrow=ncol(x), ncol=3)
+  rownames(md) <- varnames
+  colnames(md) <- c("Start", "End", "Modalities")
+  md          <- as.data.frame(md)
+  
+  for (i in 1:ncol(x)){
+    mr         <- as.numeric(mm[,2][mm[,1]==varnames[i]])
+    md[i,1]    <- min(mr)
+    md[i,2]    <- max(mr)
+    md[i,3]    <- length(mr)
+  }
+  md[,1]      <- as.numeric(md[,1])
+  md[,2]      <- as.numeric(md[,2])
+  
+  result$modal    <- md
+  
+  class(result)   <- "soc.ca"
+  return(result)
 }
 
 ################ Burt Matrix
@@ -188,128 +199,103 @@ return(fburt)
 # B is a Burt matrix
 # supmat is a supplementary burt matrix
 # nvar ???
-subset.ca   <- function(B, subset, supmat, nvar=ncol(x)){
-obj 		<- B
-nd 		<- ncol(obj)
-modal.names 	<- colnames(obj)[subset]
-N		<- matrix(as.matrix(obj), nrow = nd, ncol = nd)
-n 		<- sum(N) 				# summen af Burtmatricen
-P 		<- N/n 					# Den relative frekvens i forhold til totalen for Burt Matricen
-mass 		<- apply(P, 1, sum)			# Summen af hver r?kkes relative frekvenser eller massen
-s.mass 		<- mass[subset]
-eP 		<- mass %*% t(mass) 			# Han ganger massen med den transponerede masse - som er den samme
-eN 		<- eP * n 				# Fra relative procenter, til relative frekvenser
-S 		<- (P - eP)/sqrt(eP)			# RESIDUALMATRICEN: givet ved skaleringsproduktet - her skabes den f?lles skala, p? tv?rs af dimensioner
-S 		<- S[subset, subset]
-dec 		<- svd(S)				# Singul?r v?rdi dekomposition
-sv 		<- dec$d				# Her hentes de singul?re v?rdier (En for hver dimension)
-u 		<- dec$u				# Her er de singul?re vectorer - (En modalitets position p? samtlige dimensioner i det dekompositionerede rum)
-ev 		<- sv^2 				# EigenVEKTOR (Principal inertias)    
-totin 		<- sum(ev)				# Den totale inerti eller den samlede m?ngde variation/afstande
-inertia 	<- apply(S^2, 1, sum) 			# Inerti for hver modalitet over dimensionerne
-chidist 	<- sqrt(inertia/s.mass) 		# Chi afstande fra hver modalitet til origo/centriden
-phi 		<- as.matrix(u)/sqrt(s.mass) 		# Standard koordinaterne
-nd 		<- nd-(nd-length(subset))
+subset.ca   <- function(B, subset, supmat, nvar=ncol(x), Qm=Qm){
+  obj     <- B
+  nd 		<- ncol(obj)
+  modal.names 	<- colnames(obj)[subset]
+  N		<- matrix(as.matrix(obj), nrow = nd, ncol = nd)
+  n 		<- sum(N) 				# summen af Burtmatricen
+  P 		<- N/n 					# Den relative frekvens i forhold til totalen for Burt Matricen
+  mass 		<- apply(P, 1, sum)			# Summen af hver r?kkes relative frekvenser eller massen
+  s.mass 		<- mass[subset]
+  eP 		<- mass %*% t(mass) 			# Han ganger massen med den transponerede masse - som er den samme
+  eN 		<- eP * n 				# Fra relative procenter, til relative frekvenser
+  S 		<- (P - eP)/sqrt(eP)			# RESIDUALMATRICEN: givet ved skaleringsproduktet - her skabes den f?lles skala, p? tv?rs af dimensioner
+  S 		<- S[subset, subset]
+  dec 		<- svd(S)				# Singul?r v?rdi dekomposition
+  sv 		<- dec$d				# Her hentes de singul?re v?rdier (En for hver dimension)
+  u 		<- dec$u				# Her er de singul?re vectorer - (En modalitets position p? samtlige dimensioner i det dekompositionerede rum)
+  ev 		<- sv^2 				# EigenVEKTOR (Principal inertias)    
+  inertia 	<- apply(S^2, 1, sum) 			# Inerti for hver modalitet over dimensionerne
+  chidist 	<- sqrt(inertia/s.mass) 		# Chi afstande fra hver modalitet til origo/centriden
+  phi 		<- as.matrix(u)/sqrt(s.mass) 		# Standard koordinaterne
+  nd 		<- nd-(nd-length(subset))
+  
+  rs.sum	 	<- apply(supmat, 1, sum)		# R?kkesummen af de supplement?re
+  supmat	 	<- supmat[,subset] 			# Her fjernes subsettet p? de aktive kolonner
+  
+  supchi		<- supmat / apply(supmat, 1, sum)	# Chi afstande for de supplement?re punkter
+  supchi		<- t((t(supchi) - s.mass) / sqrt(s.mass))
+  supchidist	<- sqrt(apply(supchi^2, 1, sum))
+  chidist 	<- c(chidist, as.numeric(supchidist))
+  
+  cs		<- mass[subset]				# Coordinater for de supplement?re punkter
+  gam.00		<- phi
+  base2		<- supmat / matrix(rs.sum, nrow = nrow(supmat), ncol = ncol(supmat))
+  base2 		<- t(base2)
+  cs.0		<- matrix(cs, nrow = nrow(base2), ncol = ncol(base2))
+  svphi		<- matrix(sv[1:nd], nrow = nrow(supmat), ncol = nd, byrow = T) 
+  base2		<- base2 - cs.0
+  phi2		<- (t(as.matrix(base2)) %*% gam.00) / svphi
+  
+  phi 	 	<- rbind(phi, phi2)
+  modal.names	<- c(modal.names, rownames(supmat))
+  rownames(phi) 	<- modal.names
+  
+  # a.aktive 	<- ncol(supmat)
+  # a.sup		<- nrow(supmat)
+  # Mass og Inertia har ikke samme l?ngde som coord - fordi de ikke har NA til de supplement?re.
+  
+  
+  
+  ### Principal coordinates
+  
+  K   		  <- nd # Her udregnes principal coordinaterne
+  I   		  <- dim(phi)[1] 
+  
+  
+  svF 		  <- matrix(rep(sv, I), I, K, byrow = TRUE)
+  rpc 		  <- phi[,1:K] * svF
+  
+  
+  ####### This is the calculation of the adjusted rates of variance
+  
+  R1.dim        <- K-Qm
+  R1.eigen.val  <- sv[1:R1.dim]
+  Mean.r.eigen  <- mean(R1.eigen.val, na.rm=TRUE)
+  R2.eigen.val  <- sv[sv>=Mean.r.eigen]
+  R2.dim        <- which(R2.eigen.val >= Mean.r.eigen) # The reduced dimensions
+  unadj.var     <- 100*(sv[1:R1.dim])/sum(sv[1:R1.dim]) #Ikke adjustede procenter
+  sum.adj.var.mod <- (sv[R2.dim]-Mean.r.eigen)^2
+  sum.adj.var   <- sum(sum.adj.var.mod)
+  adj.var       <- round(((sum.adj.var.mod/sum.adj.var)*100), digits=1)
+  cumpercent	  <- cumsum(adj.var)
+  
+  adj.inertia 	<- cbind(R2.dim, round(sv[R2.dim], 3), round(unadj.var[R2.dim], 1), adj.var ,cumpercent) # De Rouanet adjustede inertier - skal nok rykkes ned.
+  colnames(adj.inertia) <- c("Dim", "Sv", "Var" ,"Adj.Var", "Cum%")
+  
+  # Reported values
+  totin   	<- sum(ev[R2.dim])				# Den totale inerti eller den samlede m?ngde variation/afstande
 
-rs.sum	 	<- apply(supmat, 1, sum)		# R?kkesummen af de supplement?re
-supmat	 	<- supmat[,subset] 			# Her fjernes subsettet p? de aktive kolonner
+  
+  # CTR
+  submass 	<- mass[subset]
+  rpcakt 		<- rpc[1:nd, 1:nd]
+  cor 		  <- rpc^2 / chidist^2
+  ctr 		  <- matrix(ncol=nd, nrow=nd)
+  for (i in 1:nd){
+    ctr[i,] 	<- as.matrix(submass * rpcakt[,i]^2 /sv[i]^2)
+  }
+  ctr 		<- t(ctr)
 
-supchi		<- supmat / apply(supmat, 1, sum)	# Chi afstande for de supplement?re punkter
-supchi		<- t((t(supchi) - s.mass) / sqrt(s.mass))
-supchidist	<- sqrt(apply(supchi^2, 1, sum))
-chidist 	<- c(chidist, as.numeric(supchidist))
-
-cs		<- mass[subset]				# Coordinater for de supplement?re punkter
-gam.00		<- phi
-base2		<- supmat / matrix(rs.sum, nrow = nrow(supmat), ncol = ncol(supmat))
-base2 		<- t(base2)
-cs.0		<- matrix(cs, nrow = nrow(base2), ncol = ncol(base2))
-svphi		<- matrix(sv[1:nd], nrow = nrow(supmat), ncol = nd, byrow = T) 
-base2		<- base2 - cs.0
-phi2		<- (t(as.matrix(base2)) %*% gam.00) / svphi
-
-phi 	 	<- rbind(phi, phi2)
-modal.names	<- c(modal.names, rownames(supmat))
-rownames(phi) 	<- modal.names
-
-# a.aktive 	<- ncol(supmat)
-# a.sup		<- nrow(supmat)
-# Mass og Inertia har ikke samme l?ngde som coord - fordi de ikke har NA til de supplement?re.
-
-K   		<- nd # Her udregnes principal coordinaterne
-I   		<- dim(phi)[1] 
-svF 		<- matrix(rep(sv, I), I, K, byrow = TRUE)
-rpc 		<- phi[,1:K] * svF
-
-
-values     <- sv^2
-values2    <- 100*(sv^2)/sum(sv^2)
-values3    <- cumsum(100*(sv^2)/sum(sv^2))
-explained.inertia  <- cbind(1:length(sv), values, values2, values3)
-colnames(explained.inertia) <- c("Dim","Sv", "%", "Cum%")
-
-sing     	<- values2
-Dim 		<- which(as.matrix(sing) >= mean(sing, na.rm=TRUE)) # Rouanet adjustment
-cumulative 	<- sing[Dim]
-percent 	<- cumulative/sum(cumulative)*100
-cumpercent	<- cumsum(cumulative/sum(cumulative)*100)
-dimension 	<- 1:length(percent)
-adj.inertia 	<- cbind(dimension, sv[Dim] ,percent,cumpercent) # De Rouanet adjustede inertier - skal nok rykkes ned.
-colnames(adj.inertia) <- c("Dim", "Sv", "%", "Cum%")
-
-
-submass 	<- mass[subset]
-rpcakt 		<- rpc[1:nd, 1:nd]
-cor 		<- rpc^2 / chidist^2
-ctr 		<- matrix(ncol=nd, nrow=nd)
-for (i in 1:nd){
-  ctr[i,] 	<- as.matrix(submass * rpcakt[,i]^2 /sv[i]^2)
+  
+  ca.output <- list(sv = sv[R2.dim], nd = nd[R2.dim], ev = ev[R2.dim], total.inertia = totin,
+                    names = modal.names, mass = mass[subset], inertia = inertia[R2.dim],
+                    adj.inertia=adj.inertia, contrib=ctr[,R2.dim], cor=cor[,R2.dim],
+                    coord = rpc[,R2.dim], standard.coord=phi[,R2.dim])
+  return(ca.output)
 }
-ctr 		<- t(ctr)
 
-#av.ctr 		<- list()
-#for (i in 1:nd){
-#  av.ctr[[i]]	<- which(ctr[,i] >= mean(ctr[,i], na.rm=TRUE))
-#}
-
-## Vi prøver at adjuste
-
-nd.max <- nd - nvar
-Q <- nvar
-cm <- mass
-cm <- mass[subset]
-J <- length(subset) # Her laver vi en Christoph adjustment af den subsettede matrice.
-#J <- length(set) Den her giver negative lambda.e
-
-# nd <- nd.max
-lambda0 <- dec$d[1:nd.max]^2
-nd.max <- sum(sqrt(lambda0) >= 1/Q)
-lambda.adj <- ((Q/(Q - 1))^2 * (sqrt(lambda0)[1:nd.max] - 1/Q)^2)
-
-# Sakset fra Greenacre 
-
-            nd.max <- sum(sqrt(lambda0) >= 1/Q)
-            if (is.na(nd) | nd > nd.max) 
-                nd <- nd.max
-            lambda.adj <- ((Q/(Q - 1))^2 * (sqrt(lambda0)[1:nd.max] - 
-                1/Q)^2)
-            lambda.t <- (Q/(Q - 1)) * (sum(lambda0) - ((J - Q)/Q^2))
-            lambda.e <- lambda.adj/lambda.t # Det her giver et tal for forklaret inerti, der ikke summer til 1.
-#            lambda.et <- NA
-            lambda0 <- lambda.adj
-            colinertia <- (Q/(Q - 1))^2 * (inertia - (1/Q) *  # Det her er ikke godt - den kræver en subsettet mass, hvilket er ok, men så spytte den negative værdier ud.
-                ((1/Q) - cm))
-            colcoord <- as.matrix(dec$v[, 1:nd.max])/sqrt(cm) # Er det samme som vi har regnet ud, which is good.
-#            rowcoord <- rowcoord[, 1:nd.max]
-
-
-
-
-    ca.output <- list(sv = sv, nd = nd, ev = ev, totin = totin , names = modal.names, mass = mass[subset], 
-        chidist = chidist, inertia = inertia, explained.inertia = explained.inertia ,
-        adj.inertia=adj.inertia, contrib=ctr, cor=cor,  coord = rpc, standard.coord=phi)
-        return(ca.output)
-}
 
 
 ################################### Indicator matrix
--- functions_description.r
+++ functions_description.r
@@ -3,81 +3,79 @@
 
 # Print objects from the soc.ca function
 print.soc.ca <- function(object){
-Nmodal       <- length(object$active) 
-Nsup     	<- length(object$sup) # S?t if ind
-Nid 		<- object$nid
-Vnames 		<- paste(rownames(object$modal), " (",object$modal[,3], ")", sep="")
-Submass 	<- round(sum(object$mass), digits=2) # Er det her i Procent aka. summer mass altid til 1.
-
-act.dim 	<- nrow(object$adj.inertia)
-dim80 		<- which.min(object$adj.inertia[,4] < 80)
-scree.dim	<- 6
-stars 		<- scree(object, scree.dim)
-adj.dim   <- 1:scree.dim
-#adj.dim 	<- object$adj.inertia[1:scree.dim,1]
-dim.a   <- ifelse((scree.dim<nrow(object$adj.inertia)), scree.dim, nrow(object$adj.inertia))
-adj <- vector(mode="numeric", length=scree.dim) # Måske skal den ikke smide 0 som output men noget bedre, når der ikke er flere dim end 6
-adj[1:dim.a] <- object$adj.inertia[1:dim.a,3]
-adj	<- paste(formatC(adj,format="f",digits=1), sep="", collide="%")
-## Tests
-pm 		<- balance.ctr(object, act.dim)
-pm.minus <- which(as.numeric(pm[,3])<0.5)
-pm.plus <- which(as.numeric(pm[,3])>2)
-
-## Output
-cat(format("Specific Correspondence Analysis:", 	width=90, justify="centre"),"\n", "\n", 
-format("Statistics", 					width=50, justify="centre"), format("Scree plot", width=40, justify="centre"),"\n",
-
-format("	Active dimensions: ", 			width=40,), format(act.dim, 	width=10, justify="right"),
-format("|  1.", width=10, justify="centre" ), format(adj[1], width=10, justify="centre"), format(paste(stars[[1]]), width=1), "\n",
-
-format("	Dimensions explaining 80% of inertia: ",width=40,), format(dim80, 	width=10, justify="right"), 
-format("|  2.", width=10, justify="centre" ), format(adj[2], width=10, justify="centre"), format(paste(stars[[2]]), width=1), "\n",
-
-format("	Active modalities: ", 			width=40,), format(Nmodal, 	width=10, justify="right"), 
-format("|  3.", width=10, justify="centre" ), format(adj[3], width=10, justify="centre"), format(paste(stars[[3]]), width=1), "\n",
-
-format("	Supplementary modalities: ",		width=40,), format(Nsup, 	width=10, justify="right"), 
-format("|  4.", width=10, justify="centre" ), format(adj[4], width=10, justify="centre"), format(paste(stars[[4]]), width=1), "\n",
-
-format("	Individuals: ",		 		width=40,), format(Nid, 	width=10, justify="right"), 
-format("|  5.", width=10, justify="centre" ), format(adj[5], width=10, justify="centre"), format(paste(stars[[5]]), width=1), "\n",
-
-format("	Mass in subset",	 		width=40,), format(Submass, 	width=10, justify="right"), 
-format("|  6.", width=10, justify="centre" ), format(adj[6], width=10, justify="centre"), format(paste(stars[[6]]), width=1), "\n",
-
-"\n",
-format("The active variables:", 			width=100, justify="centre" ),
-"\n",
-sep="")
-cat(format(Vnames, width=25, justify="right"), fill=100)
-
-# Test
-
-
-if (length(pm.plus)>0){
-  p.out <- matrix(, ncol=3, nrow=length(pm.plus))
-  p.out[,] <- formatC(pm[pm.plus,], format="f", digits=2)
-  rownames(p.out) <- rep(format("", width=35), times=length(pm.plus))  
-  p.out <- cbind(paste(pm.plus, ".", sep=""), p.out)
-  colnames(p.out) <- c("Dim.", "+", "-", "+/-")
-  cat("\n",format("These dimensions contributions are skewed towards (+): ", width=100, justify="centre"), "\n")
-  print(p.out, quote=FALSE, right=TRUE)
-}      
-
-if (length(pm.minus)>0){
-  p.out <- matrix(, ncol=3, nrow=length(pm.minus))
-  p.out[,] <- formatC(pm[pm.minus,], format="f", digits=2)
-  rownames(p.out) <- rep(format("", width=35), times=length(pm.minus))
-  p.out <- cbind(paste(pm.minus, ".", sep=""), p.out)
-  colnames(p.out) <- c("Dim.", "+", "-", "+/-")
-  cat("\n",format("These dimensions contributions are skewed towards (-): ", width=100, justify="centre"), "\n")
-  print(p.out, quote=FALSE, right=TRUE)
+  Nmodal       <- length(object$active) 
+  Nsup       <- length(object$sup) # S?t if ind
+  Nid   	<- object$nid
+  Vnames 		<- paste(rownames(object$modal), " (",object$modal[,3], ")", sep="")
+  Submass 	<- round(sum(object$mass), digits=2) # Er det her i Procent aka. summer mass altid til 1.
+  
+  act.dim 	<- nrow(object$adj.inertia)
+  dim80 		<- which.min(object$adj.inertia[,5] < 80)
+  scree.dim	<- 6
+  stars 		<- scree(object, scree.dim)
+  adj.dim   <- 1:scree.dim
+  #adj.dim 	<- object$adj.inertia[1:scree.dim,1]
+  dim.a   <- ifelse((scree.dim<nrow(object$adj.inertia)), scree.dim, nrow(object$adj.inertia))
+  adj <- vector(mode="numeric", length=scree.dim) # Måske skal den ikke smide 0 som output men noget bedre, når der ikke er flere dim end 6
+  adj[1:dim.a] <- object$adj.inertia[1:dim.a,4]
+  adj	<- paste(formatC(adj,format="f",digits=1), sep="", collide="%")
+  ## Tests
+  pm 		<- balance.ctr(object, act.dim)
+  pm.minus <- which(as.numeric(pm[,3])<0.5)
+  pm.plus <- which(as.numeric(pm[,3])>2)
+  
+  ## Output
+  cat(format("Specific Correspondence Analysis:", 	width=90, justify="centre"),"\n", "\n", 
+      format("Statistics", 					width=50, justify="centre"), format("Scree plot", width=40, justify="centre"),"\n",
+      
+      format("	Active dimensions: ", 			width=40,), format(act.dim, 	width=10, justify="right"),
+      format("|  1.", width=10, justify="centre" ), format(adj[1], width=10, justify="centre"), format(paste(stars[[1]]), width=1), "\n",
+      
+      format("	Dimensions explaining 80% of inertia: ",width=40,), format(dim80, 	width=10, justify="right"), 
+      format("|  2.", width=10, justify="centre" ), format(adj[2], width=10, justify="centre"), format(paste(stars[[2]]), width=1), "\n",
+      
+      format("	Active modalities: ", 			width=40,), format(Nmodal, 	width=10, justify="right"), 
+      format("|  3.", width=10, justify="centre" ), format(adj[3], width=10, justify="centre"), format(paste(stars[[3]]), width=1), "\n",
+      
+      format("	Supplementary modalities: ",		width=40,), format(Nsup, 	width=10, justify="right"), 
+      format("|  4.", width=10, justify="centre" ), format(adj[4], width=10, justify="centre"), format(paste(stars[[4]]), width=1), "\n",
+      
+      format("	Individuals: ",		 		width=40,), format(Nid, 	width=10, justify="right"), 
+      format("|  5.", width=10, justify="centre" ), format(adj[5], width=10, justify="centre"), format(paste(stars[[5]]), width=1), "\n",
+      
+      format("	Mass in subset",	 		width=40,), format(Submass, 	width=10, justify="right"), 
+      format("|  6.", width=10, justify="centre" ), format(adj[6], width=10, justify="centre"), format(paste(stars[[6]]), width=1), "\n",
+      
+      "\n",
+      format("The active variables:", 			width=100, justify="centre" ),
+      "\n",
+      sep="")
+  cat(format(Vnames, width=25, justify="right"), fill=100)
+  
+  # Test
+  
+  
+  if (length(pm.plus)>0){
+    p.out <- matrix(, ncol=3, nrow=length(pm.plus))
+    p.out[,] <- formatC(pm[pm.plus,], format="f", digits=2)
+    rownames(p.out) <- rep(format("", width=35), times=length(pm.plus))  
+    p.out <- cbind(paste(pm.plus, ".", sep=""), p.out)
+    colnames(p.out) <- c("Dim.", "+", "-", "+/-")
+    cat("\n",format("These dimensions contributions are skewed towards (+): ", width=100, justify="centre"), "\n")
+    print(p.out, quote=FALSE, right=TRUE)
+  }      
+  
+  if (length(pm.minus)>0){
+    p.out <- matrix(, ncol=3, nrow=length(pm.minus))
+    p.out[,] <- formatC(pm[pm.minus,], format="f", digits=2)
+    rownames(p.out) <- rep(format("", width=35), times=length(pm.minus))
+    p.out <- cbind(paste(pm.minus, ".", sep=""), p.out)
+    colnames(p.out) <- c("Dim.", "+", "-", "+/-")
+    cat("\n",format("These dimensions contributions are skewed towards (-): ", width=100, justify="centre"), "\n")
+    print(p.out, quote=FALSE, right=TRUE)
+  }
+  
 }
-
-}
-
-
 
 #################### Balance measure for contributing modalities
 # Object is a soc.ca class object
@@ -108,16 +106,17 @@ return(pm)
 # Dim is the number of dimensions included in the plot
 
 scree <- function(object, dim=6){
-set.dim  <- dim
-dim <- ifelse((nrow(object$adj.inertia)<dim)==TRUE, nrow(object$adj.inertia), dim)
-adj <- round(object$adj.inertia[1:dim,3], digits=1)
-stars <- round(round(adj)/2)
-starscree <- vector("list", set.dim)
-for (i in 1:length(stars)){
-starscree[[i]] <- noquote(rep("*", stars[i]))
+  set.dim  <- dim
+  dim <- ifelse((nrow(object$adj.inertia)<dim)==TRUE, nrow(object$adj.inertia), dim)
+  adj <- round(object$adj.inertia[1:dim,4], digits=1)
+  stars <- round(round(adj)/2)
+  starscree <- vector("list", set.dim)
+  for (i in 1:length(stars)){
+    starscree[[i]] <- noquote(rep("*", stars[i]))
+  }
+  return(starscree)
 }
-return(starscree)
-}
+
 
 # En funktion der undersøger om der er modaliteter der styrer en dimension for meget. 
 #Kritik - den vil spytte noget ud i et væk, hvis ikke den holdes nede til kun at undersøge et meget begrænset subset. Måske skal den gemmes til senere.
