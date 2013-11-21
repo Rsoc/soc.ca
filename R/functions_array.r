c9b969f1fed12091887a2fc36ee20ac94cf52d5e
antongrau <alar@soc.ku.dk>
2013-11-21 13:18:34 +0100

    finally

--- R/functions_array.r
+++ R/functions_array.r
@@ -70,8 +70,8 @@ map.ellipse.array <- function(object, variable, dim=c(1,2), draw.ellipses=TRUE, 
   # Standardize the coordinates
   list.of.maps <- fix.coords(list.of.maps)
   # Plot the maps
-  return(invisible(list.of.maps))
   map.array(list.of.maps, ncol=ncol, title=main.title)
+  return(invisible(list.of.maps))
 }
 
 #################################################################################################
